e0183ecea2605fcc649d1ed4299599d473b2be7f
ayush <ayushshukla452001@gmail.com>
2023-04-28 18:25:49 +0530

    https://github.com/ayush997769/Quiz-app.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const questions = [
+		{
+			questionText: 'Olympics games are held after every?',
+			answerOptions: [
+				{ answerText: '6 years', isCorrect: false },
+				{ answerText: '3 years', isCorrect: false },
+				{ answerText: '4 years', isCorrect: true },
+				{ answerText: '5 years', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is the Founder of Facebook?',
+			answerOptions: [
+				{ answerText: 'Elon mask', isCorrect: false },
+				{ answerText: 'Mark Zuckerberg', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many days are there in a leap year?',
+			answerOptions: [
+				{ answerText: '366', isCorrect: true },
+				{ answerText: '364', isCorrect: false },
+				{ answerText: '365', isCorrect: false },
+				{ answerText: '367', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'which is the smallest state of india?',
+			answerOptions: [
+				{ answerText: 'Rajasthan', isCorrect: false },
+				{ answerText: 'Uttar pradesh', isCorrect: false },
+				{ answerText: 'Punjab', isCorrect: false },
+				{ answerText: 'Goa', isCorrect: true },
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-
-export default App;
